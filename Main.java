fa91a9d4dc74c9860c91b76270ac611c502b72d2
Kajeloki <101186819+Kajeloki@users.noreply.github.com>
2022-03-08 12:14:52 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) 
+  {
+    String imie="Piotr";
+    System.out.println("Witaj "+imie+"!");
+    System.out.println("Test math:");
+    int a=2;
+    int b=2;
+    int suma=a+b;
+    System.out.println(a+"+"+b+"="+suma);
+  }
+}
